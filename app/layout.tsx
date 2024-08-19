6653f1da9e346261e14c5d74c1a4ea16fa6226bb
Priest <Charleschukwuemeka47@gmail.com>
2024-08-19 22:02:46 +0100

    Fix: Rename folder to stylesheets inorder to solve error issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.css";
+
+//Import Needed Styles
+import "../stylesheets/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
